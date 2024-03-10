cfe3588fbcbae0cf4bf72a2103908c2dd42d0398
Visa Pollari <visa.pollari@gmail.com>
2024-03-10 13:37:40 +0200

    template endpoints

---
+++ app/[id]/fiilis/[postId]/route.ts
@@ -0,0 +1,18 @@
+
+import { NextResponse } from 'next/server';
+
+type Props = {
+    params: { id: string, postId: string};
+  };
+  
+export async function GET(req: Request, { params }: Props) {
+    // get specific fiilis for the user with the given id
+    return NextResponse.json({
+        message: "Get fiilis ",id: params.id, postId: params.postId });
+}
+
+export async function PATCH(req: Request, { params }: Props) {
+    // update the fiilis with the given id
+    return NextResponse.json({ message: "Patch fiilis ",id: params.id, postId: params.postId });
+}
+
