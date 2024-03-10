f9e4b9cbd291c4cc90dfeeca38ff36868948e7ba
Visa Pollari <visa.pollari@gmail.com>
2024-03-10 16:19:18 +0200

    post fiilis endpoint

--- app/[id]/fiilis/route.ts
+++ app/[id]/fiilis/route.ts
@@ -1,28 +1,69 @@
 import { NextResponse } from 'next/server';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
-type Props = {
-  params: { id: string };
-};
+// Initialize DynamoDB Document Client
+const docClient = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    region: process.env.AWS_DEFAULT_REGION || 'eu-north-1',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+    },
+  }),
+);
 
-export async function GET(req: Request, { params }: Props) {
-  // get all the data for the user with the given id
-  return NextResponse.json({
-    message: 'Get fiilis ',
-    id: params.id,
+export async function POST(req: Request) {
+  // Parse the JSON body from the request
+  const body = await req.json();
+  const { year, guild, campus, score } = body;
+
+  // Generate a timestamp and a random number for the partition_key
+  const timestamp = Math.floor(Date.now() / 1000);
+  const rand = Math.floor(Math.random() * 100000);
+  console.log('Adding item to DynamoDB:', {
+    year,
+    guild,
+    campus,
+    score,
+    timestamp,
   });
-}
-
-export async function POST(req: Request, { params }: Props) {
-  // post a new datapoint for the user with the given id
-  return NextResponse.json({ message: 'Post fiilis ', id: params.id });
-}
-
-export async function PUT(req: Request, { params }: Props) {
-  // update the user with the given id
-  return NextResponse.json({ message: 'Put fiilis ', id: params.id });
-}
-
-export async function DELETE(req: Request, { params }: Props) {
-  // delete the user with the given id
-  return NextResponse.json({ message: 'Delete fiilis ', id: params.id });
+  try {
+    const res = await docClient.send(
+      new PutCommand({
+        TableName: process.env.DYNAMODB_EVENTS_TABLE_NAME,
+        Item: {
+          partition_key: `${year}::${guild}::${rand}`,
+          year: year,
+          guild: guild,
+          campus: campus,
+          score: score,
+          timestamp: timestamp,
+        },
+      }),
+    );
+    console.log('Item added to DynamoDB:', res);
+    // Return a success response
+    return NextResponse.json(
+      { message: 'Item added successfully' },
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  } catch (error) {
+    console.error('Error adding item to DynamoDB:', error);
+    // Return an error response
+    return NextResponse.json(
+      { message: 'Failed to add item' },
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+  }
 }
