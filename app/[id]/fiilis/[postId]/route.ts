c248f7fc3c14337415d29f50a9aca66326c334fb
Visa Pollari <visa.pollari@gmail.com>
2024-03-10 15:06:37 +0200

    lint

--- app/[id]/fiilis/[postId]/route.ts
+++ app/[id]/fiilis/[postId]/route.ts
@@ -1,18 +1,23 @@
-
 import { NextResponse } from 'next/server';
 
 type Props = {
-    params: { id: string, postId: string};
-  };
-  
+  params: { id: string; postId: string };
+};
+
 export async function GET(req: Request, { params }: Props) {
-    // get specific fiilis for the user with the given id
-    return NextResponse.json({
-        message: "Get fiilis ",id: params.id, postId: params.postId });
+  // get specific fiilis for the user with the given id
+  return NextResponse.json({
+    message: 'Get fiilis ',
+    id: params.id,
+    postId: params.postId,
+  });
 }
 
 export async function PATCH(req: Request, { params }: Props) {
-    // update the fiilis with the given id
-    return NextResponse.json({ message: "Patch fiilis ",id: params.id, postId: params.postId });
+  // update the fiilis with the given id
+  return NextResponse.json({
+    message: 'Patch fiilis ',
+    id: params.id,
+    postId: params.postId,
+  });
 }
-
--- app/[id]/fiilis/route.ts
+++ app/[id]/fiilis/route.ts
@@ -1,26 +1,28 @@
 import { NextResponse } from 'next/server';
 
 type Props = {
-    params: { id: string };
-  };
-  
+  params: { id: string };
+};
+
 export async function GET(req: Request, { params }: Props) {
-    // get all the data for the user with the given id
-    return NextResponse.json({
-        message: "Get fiilis ",id: params.id });
+  // get all the data for the user with the given id
+  return NextResponse.json({
+    message: 'Get fiilis ',
+    id: params.id,
+  });
 }
 
 export async function POST(req: Request, { params }: Props) {
-    // post a new datapoint for the user with the given id
-    return NextResponse.json({ message: "Post fiilis ",id: params.id });
+  // post a new datapoint for the user with the given id
+  return NextResponse.json({ message: 'Post fiilis ', id: params.id });
 }
 
 export async function PUT(req: Request, { params }: Props) {
-    // update the user with the given id
-    return NextResponse.json({ message: "Put fiilis ",id: params.id });
+  // update the user with the given id
+  return NextResponse.json({ message: 'Put fiilis ', id: params.id });
 }
 
 export async function DELETE(req: Request, { params }: Props) {
-    // delete the user with the given id
-    return NextResponse.json({ message: "Delete fiilis ",id: params.id });
+  // delete the user with the given id
+  return NextResponse.json({ message: 'Delete fiilis ', id: params.id });
 }
