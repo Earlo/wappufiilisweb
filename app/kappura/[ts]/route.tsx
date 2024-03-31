7d57fc67446229313d88c47a915451c8fdc02cfe
Ilmari Tarpila <ilmari.tarpila@upsyshopping.com>
2024-03-31 23:52:32 +0300

    fix: wrong table name

--- app/kappura/[ts]/route.tsx
+++ app/kappura/[ts]/route.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export async function GET(req: Request, { params }: Props) {
   const tableName =
-    process.env.DYNAMODB_EVENTS_PER_GUILD_TABLE_NAME || 'FiilisData_staging';
+    process.env.DYNAMODB_EVENTS_TABLE_NAME || 'FiilisData_staging';
 
   const entries = await listAllData(tableName, '');
   const data = groupEntriesByDay(entries).sort((a, b) => a.date - b.date);
